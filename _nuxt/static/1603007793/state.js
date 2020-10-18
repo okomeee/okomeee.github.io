70ff326670bab1cdc20449390b772e5847bea145
Katsuhiro Mochida <kome.o.kun@gmail.com>
2020-10-18 16:57:35 +0900

    Update Portfolio

---
+++ _nuxt/static/1603007793/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1603007793",layout:"default",error:null,state:{bgcolor:"#00162e",accolor:a,colorList:[a,b],flashColorList:[a,b,a,b,a,b,a,b],duration:500,delay:1500,rotateDuration:c,perDelay:100,titleOffset:c},serverRendered:true,routePath:"\u002F",config:{}}}("#52F2C5","#05AFF2",1000));
