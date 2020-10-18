90879f681db59db79ed81d72a840f7ae649d4416
Katsuhiro Mochida <kome.o.kun@gmail.com>
2020-10-18 17:08:27 +0900

    Update Portfolio

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 const options = {
   workboxURL:
     'https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.3/workbox/workbox-sw.js',
@@ -40,18 +38,9 @@ const options = {
   pagesURLPattern: '/',
   offlineStrategy: 'NetworkFirst',
 }
-=======
-importScripts(
-  'https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.3/workbox/workbox-sw.js'
-)
->>>>>>> parent of 4c199a7... Update Portfolio
-=======
-const options = {"workboxURL":"https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.3/workbox/workbox-sw.js","importScripts":[],"config":{"debug":false},"clientsClaim":true,"skipWaiting":true,"cleanupOutdatedCaches":true,"offlineAnalytics":false,"preCaching":["/","/"],"runtimeCaching":[{"urlPattern":"/_nuxt/","handler":"CacheFirst","method":"GET","strategyPlugins":[]},{"urlPattern":"/","handler":"NetworkFirst","method":"GET","strategyPlugins":[]},{"urlPattern":"/_nuxt/","handler":"CacheFirst","method":"GET","strategyPlugins":[]},{"urlPattern":"/","handler":"NetworkFirst","method":"GET","strategyPlugins":[]}],"offlinePage":null,"pagesURLPattern":"/","offlineStrategy":"NetworkFirst"}
->>>>>>> parent of 8e572d1... Update Portfolio
 
 importScripts(...[options.workboxURL, ...options.importScripts])
 
-<<<<<<< HEAD
 initWorkbox(workbox, options)
 workboxExtensions(workbox, options)
 precacheAssets(workbox, options)
@@ -59,12 +48,6 @@ cachingExtensions(workbox, options)
 runtimeCaching(workbox, options)
 offlinePage(workbox, options)
 routingExtensions(workbox, options)
-=======
-// Set workbox config
-workbox.setConfig({
-  debug: false,
-})
->>>>>>> parent of 4c199a7... Update Portfolio
 
 function getProp(obj, prop) {
   return prop.split('.').reduce((p, c) => p[c], obj)
@@ -94,7 +77,6 @@ function initWorkbox(workbox, options) {
     workbox.precaching.cleanupOutdatedCaches()
   }
 
-<<<<<<< HEAD
   if (options.offlineAnalytics) {
     // Enable offline Google Analytics tracking
     workbox.googleAnalytics.initialize()
@@ -103,7 +85,10 @@ function initWorkbox(workbox, options) {
 
 function precacheAssets(workbox, options) {
   if (options.preCaching.length) {
-    workbox.precaching.precacheAndRoute(options.preCaching, options.cacheOptions)
+    workbox.precaching.precacheAndRoute(
+      options.preCaching,
+      options.cacheOptions
+    )
   }
 }
 
@@ -112,8 +97,9 @@ function runtimeCaching(workbox, options) {
     const urlPattern = new RegExp(entry.urlPattern)
     const method = entry.method || 'GET'
 
-    const plugins = (entry.strategyPlugins || [])
-      .map(p => new (getProp(workbox, p.use))(...p.config))
+    const plugins = (entry.strategyPlugins || []).map(
+      (p) => new (getProp(workbox, p.use))(...p.config)
+    )
 
     const strategyOptions = { ...entry.strategyOptions, plugins }
 
@@ -126,40 +112,20 @@ function runtimeCaching(workbox, options) {
 function offlinePage(workbox, options) {
   if (options.offlinePage) {
     // Register router handler for offlinePage
-    workbox.routing.registerRoute(new RegExp(options.pagesURLPattern), ({ request, event }) => {
-      const strategy = new workbox.strategies[options.offlineStrategy]
-      return strategy
-        .handle({ request, event })
-        .catch(() => caches.match(options.offlinePage))
-    })
+    workbox.routing.registerRoute(
+      new RegExp(options.pagesURLPattern),
+      ({ request, event }) => {
+        const strategy = new workbox.strategies[options.offlineStrategy]()
+        return strategy
+          .handle({ request, event })
+          .catch(() => caches.match(options.offlinePage))
+      }
+    )
   }
 }
 
-function workboxExtensions(workbox, options) {
-  
-}
+function workboxExtensions(workbox, options) {}
 
-function cachingExtensions(workbox, options) {
-  
-}
+function cachingExtensions(workbox, options) {}
 
-<<<<<<< HEAD
 function routingExtensions(workbox, options) {}
-=======
-// Register route handlers for runtimeCaching
-workbox.routing.registerRoute(
-  new RegExp('/PortfolioNuxtJS/_nuxt/'),
-  new workbox.strategies.CacheFirst({}),
-  'GET'
-)
-workbox.routing.registerRoute(
-  new RegExp('/PortfolioNuxtJS/'),
-  new workbox.strategies.NetworkFirst({}),
-  'GET'
-)
->>>>>>> parent of 4c199a7... Update Portfolio
-=======
-function routingExtensions(workbox, options) {
-  
-}
->>>>>>> parent of 8e572d1... Update Portfolio
